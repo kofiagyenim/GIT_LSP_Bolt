761d6d5d3865d12c9ef3d00650afe7952eacd04d
kofithemerc <kofithemerc@kofis-air>
2024-03-01 15:22:48 -0500

    Assignment 4

---
+++ Large-Scale-Programming/src/org/howard/edu/lsp/assignment4/Driver.java
@@ -0,0 +1,84 @@
+package org.howard.edu.lsp.assignment4;
+
+public class Driver {
+
+	// main function
+	public static void main(String[] args) {
+
+		// creating sets and adding values
+		IntegerSet set1 = new IntegerSet();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+
+		IntegerSet set2 = new IntegerSet();
+		set2.add(3);
+		set2.add(4);
+		set2.add(5);
+		set2.add(6);
+
+		IntegerSet set3 = new IntegerSet();
+
+		// printing values of the sets
+		System.out.println("Value of Set1 is: " + set1.toString());
+		System.out.println("Value of Set2 is: " + set2.toString());
+
+		// printing length of set1 and checking if set1 equals set2
+		System.out.println("The length of set1: " + set1.length());
+		System.out.println("Does set1 equal set2: "+ set1.equals(set2));
+
+		// removing value from set2 and finding difference between set2 and set1
+		set2.remove(3);
+		System.out.println("Value of set2 after removing 3 is: " + set2.toString());
+		set2.diff(set1);
+		System.out.println("The Value of set2 after finding the difference from set1: " + set2.toString());
+
+		// checking if set1 contains 2 and union of set1 and set2
+		System.out.println("Does set1 contain 2: "+ set1.contains(2));
+		set2.union(set1);
+		System.out.println("The Value of set2 after Union with set1: " + set2.toString());
+
+		// finding smallest and largest value of set1 and intersecting set1 with set2
+		try {
+			System.out.println("Smallest value in Set1 is:" + set1.smallest());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		try {
+			System.out.println("Largest value in Set1 is:" + set1.largest());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		set1.intersect(set2);
+		System.out.println("The Value of set1 after intersecting with set2: " + set1.toString());
+
+
+		// clearing set1 and checking if it is empty
+		set1.clear();
+		System.out.println("Is set1 empty after calling the function clear: "+ set1.isEmpty());	
+
+		// Test complement
+		set1.clear();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		set1.complement(set2);
+		System.out.println("Complement of Set1 relative to Set2: " + set1);
+
+		// Test largest and smallest on empty set (should throw exceptions)
+		try {
+			System.out.println("Attempting to get largest on empty Set3: " + set3.largest());
+		} catch (IntegerSetException e) {
+			System.out.println("Correctly caught exception when getting largest on empty set: " + e.getMessage());
+		}
+
+		try {
+			System.out.println("Attempting to get smallest on empty Set3: " + set3.smallest());
+		} catch (IntegerSetException e) {
+			System.out.println("Correctly caught exception when getting smallest on empty set: " + e.getMessage());
+		}
+
+		// Test equals with a non-IntegerSet object
+		System.out.println("Set1 equals a String object: " + set1.equals("Hello"));
+	}
+}
---
+++ Large-Scale-Programming/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -0,0 +1,159 @@
+package org.howard.edu.lsp.assignment4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerSet {
+    private List<Integer> set = new ArrayList<Integer>();
+
+    /**
+     * Default Constructor
+     */
+    public IntegerSet() {
+    }
+
+    // Constructor if you want to pass in already initialized
+    public IntegerSet(ArrayList<Integer> set) {
+        this.set = set;
+    }
+
+    /**
+     *  Clears the internal representation of the set
+     */
+    public void clear() {
+        set.clear();
+    }
+
+    /**
+     *  Returns the length of the set
+     * @return integer
+     */
+    
+    public int length() {
+        return set.size();
+    }
+
+    /**
+     * Returns true if the 2 sets are equal, false otherwise;
+     * Two sets are equal if they contain all of the same values in ANY order.
+     * @param set An IntegerSet Type
+     * @return Boolean
+     */
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        IntegerSet that = (IntegerSet) o;
+        return set.equals(that.set);
+    }
+
+    /**
+     *  Returns true if the set contains the value, otherwise false
+     * @param value Integer
+     * @return Boolean
+     */
+    public boolean contains(int value) {
+        return set.contains(value);
+    }
+
+    /**
+     *  Returns the largest item in the set; Throws a IntegerSetException if the set is empty 
+     * @return Integer
+     * @throws Exception
+     */
+    public int largest() throws IntegerSetException {
+        if (set.isEmpty())
+            throw new IntegerSetException("Set is empty");
+        int max = Integer.MIN_VALUE;
+        for (int num : set) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    /**
+     *  Returns the smallest item in the set; Throws a IntegerSetException if the set is empty
+     * @return Integer
+     * @throws Exception
+     */
+    public int smallest() throws IntegerSetException {
+        if (set.isEmpty())
+            throw new IntegerSetException("Set is empty");
+        int min = Integer.MAX_VALUE;
+        for (int num : set) {
+            min = Math.min(min, num);
+        }
+        return min;
+    }
+
+    /**
+     *  Adds an item to the set or does nothing it already there	
+     * @param item An integer
+     */
+    public void add(int item) {
+        if (!set.contains(item)) {
+            set.add(item);
+        }
+    }
+
+    /**
+     * Removes an item from the set or does nothing if not there
+     * @param item An integer
+     */
+    public void remove(int item) {
+        set.remove((Integer) item);
+    }
+
+    /**
+     *  Set union
+     * @param setb
+     */
+    public void union(IntegerSet intSetb) {
+        for (int num : intSetb.set) {
+            add(num);
+        }
+    }
+
+    /**
+     *  Set intersection
+     * @param setb An IntegerSet Type
+     */
+    public void intersect(IntegerSet intSetb) {
+        set.retainAll(intSetb.set);
+    }
+
+    /**
+     *  Set difference, i.e., seta – setb
+     * @param set2 An IntegerSet Type
+     */
+    public void diff(IntegerSet intSetb) {
+        set.removeAll(intSetb.set);
+    }
+
+    /**
+     *  Set Complement
+     * @param setb An IntegerSet Type
+     */
+    public void complement(IntegerSet intSetb) {
+        IntegerSet tempSet = new IntegerSet();
+        tempSet.set.addAll(set);
+        tempSet.diff(intSetb);
+        set = tempSet.set;
+    }
+
+    /**
+     * Returns true if the set is empty, false otherwise
+     * @return Boolean
+     */
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    /**
+     *  Return String representation of your set
+     */
+    public String toString() {
+        return set.toString();
+    }
+}
